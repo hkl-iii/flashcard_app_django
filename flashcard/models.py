84bc23d127ab419d2dfa0e3de29e40c30f05c866
hkl-iii <rahleggon@gmail.com>
2021-06-06 18:46:39 -0500

    updated models

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Card(models.Model):
+    number = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+class FlashCard(models.Model):
+    card =  models.ForeignKey(Card,on_delete=models.CASCADE,null=True,blank=True)
+    number = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+
+        return f' {self.card} - {self.number} '
+
+class Collection(models.Model):
+    name =  models.CharField(max_length=30,null=True,blank=True)
+    flashcard =  models.ManyToManyField(FlashCard,null=True,blank=True)
+    
+
+    def __str__(self):
+        return self.name
