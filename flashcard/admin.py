e2ffa83efbf43f7f10ff3bd198b61781bc663b56
hkl-iii <rahleggon@gmail.com>
2021-07-14 18:09:12 -0500

    updated and fixed django backend

--- flashcard/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Card)
-admin.site.register(FlashCard)
-admin.site.register(Collection)
--- flashcard/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlashcardConfig(AppConfig):
-    name = 'flashcard'
